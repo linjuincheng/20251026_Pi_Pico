1aadf1be62a0e66a9467de21dc274ee37e64e118
linjuincheg <linjuincheng@gmail.com>
2025-12-07 16:14:23 +0800

    modify

--- lesson6/app_flask.py
+++ lesson6/app_flask.py
@@ -11,13 +11,14 @@ import json
 import threading
 import csv
 import os
+
 app = Flask(__name__)
 socketio = SocketIO(app, cors_allowed_origins="*")
 
 # MQTT 設定
-MQTT_BROKER = "localhost"
+MQTT_BROKER = "192.168.163.7"
 MQTT_PORT = 1883
-MQTT_TOPIC = "客廳/感測器"
+MQTT_TOPIC = "living_room/sensor"
 
 # 全域數據儲存
 sensor_data = []
